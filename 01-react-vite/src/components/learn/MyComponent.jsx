b2de23c60db546427827ef40cd015c4a421dd09b
tuananh1006 <tuananhhocai@gmail.com>
2024-11-05 23:05:05 +0700

    Parse Parameters to JSX

--- 01-react-vite/src/components/learn/MyComponent.jsx
+++ 01-react-vite/src/components/learn/MyComponent.jsx
@@ -1,10 +1,16 @@
 import "./style.css";
-
+const Person = {
+  name: "Tuan Anh",
+  age: 20,
+  position: "Dev",
+};
 const MyComponent = () => {
   return (
     <>
-      <div className="child">Tuan anh hihi</div>
-      <div style={{ color: "green" }}>Tuan anh haha</div>
+      <div>Dang viet ve {JSON.stringify(Person)}</div>
+      <div className="child">{Person.name} dep trai</div>
+      <div style={{ color: "green" }}>Nam nay {Person.age} tuoi</div>
+      <div style={{ color: "green" }}>Hien tai dang lam {Person.position}</div>
     </>
   );
 };
