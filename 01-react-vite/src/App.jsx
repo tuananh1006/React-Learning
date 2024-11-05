ef041725226314769c36f567e2c473f3688861f6
tuananh1006 <tuananhhocai@gmail.com>
2024-11-05 22:45:55 +0700

    Add folder component

--- 01-react-vite/src/App.jsx
+++ 01-react-vite/src/App.jsx
@@ -2,10 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import MyComponent from "./components/learn/MyComponent";
 
-const HeaderNavBar = () => {
-  return <div>Tuan anh dep trai</div>;
-};
 const App = () => {
   const [count, setCount] = useState(0);
 
@@ -30,10 +28,7 @@ const App = () => {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        <HeaderNavBar />
-        <HeaderNavBar />
-        <HeaderNavBar />
-        <HeaderNavBar />
+        <MyComponent></MyComponent>
       </p>
     </>
   );
---
+++ 01-react-vite/src/components/learn/MyComponent.jsx
@@ -0,0 +1,5 @@
+const MyComponent = () => {
+  return <div>Tuan anh dep trai</div>;
+};
+
+export default MyComponent;
