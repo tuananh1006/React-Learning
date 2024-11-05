fb074500352c66583f406502ff4b87ab7c66319a
tuananh1006 <tuananhhocai@gmail.com>
2024-11-05 22:55:51 +0700

    Add file css to component

--- 01-react-vite/src/components/learn/MyComponent.jsx
+++ 01-react-vite/src/components/learn/MyComponent.jsx
@@ -1,5 +1,12 @@
+import "./style.css";
+
 const MyComponent = () => {
-  return <div>Tuan anh dep trai</div>;
+  return (
+    <>
+      <div className="child">Tuan anh hihi</div>
+      <div style={{ color: "green" }}>Tuan anh haha</div>
+    </>
+  );
 };
 
 export default MyComponent;
