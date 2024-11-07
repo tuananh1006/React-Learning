136cc1f1349e8497df148253717646b94ee2ef8f
tuananh1006 <tuananhhocai@gmail.com>
2024-11-07 20:34:52 +0700

    Add many routes

--- 01-react-vite/src/main.jsx
+++ 01-react-vite/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <div>Login ne</div>,
+  },
+  {
+    path: "/user",
+    element: <div>User ne</div>,
+  },
+  {
+    path: "/product",
+    element: <div>product ne</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
